478973359a05e5fbaa407abcc784f240bb39e197
DominarGarak <elimgarakstuff@gmail.com>
2017-09-25 09:37:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
 ﻿<?php echo "hello world"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+ 
+  <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/css/uikit.min.css" />
+
+<!-- jQuery is required -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit-icons.min.js"></script>
+  
+  <title>JS LOGIN</title>
+</head>
+  
+  
+<body>
+
+  
+  
+  
+  
+  
+</body>
+</html>
+
+
+
+
+
+
